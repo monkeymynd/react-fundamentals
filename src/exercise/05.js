3ea8ce57e279ab7dd8fe8518b8e86b7f7ec370a7
Stacey R. Brodsky <monkeymynd@gmail.com>
2020-10-06 12:37:54 -0500

    Completed react-fundamentals

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,8 +1,8 @@
 // Styling
 // http://localhost:3000/isolated/exercise/05.js
 
-import React from 'react'
-import '../box-styles.css'
+import React from 'react';
+import '../box-styles.css';
 
 // 💰 Use the className for the size and style (backgroundColor) for the color
 // 💰 each of the elements should also have the "box" className applied
@@ -12,9 +12,65 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+// const smallBox = (
+//                     <div 
+//                       className="box box--small" 
+//                       style={{fontStyle: 'italic', backgroundColor: 'lightblue'}}
+//                     >
+//                       small lightblue box
+//                     </div>
+//                   )
+// const mediumBox = (
+//                     <div 
+//                       className="box box--medium" 
+//                       style={{fontStyle: 'italic', backgroundColor: 'pink'}}
+//                     >
+//                       medium pink box
+//                     </div>
+//                   )
+// const largeBox = (
+//                     <div 
+//                       className="box box--large" 
+//                       style={{fontStyle: 'italic', backgroundColor: 'orange'}}
+//                     >
+//                       large orange box
+//                     </div>
+//                   )
+
+const smallBox = (
+  <Box 
+    size="small" 
+    style={{backgroundColor: 'lightblue'}}
+  >
+    small lightblue box
+  </Box>
+)
+const mediumBox = (
+  <Box 
+    size="medium" 
+    style={{backgroundColor: 'pink'}}
+  >
+    medium pink box
+  </Box>
+)
+const largeBox = (
+  <Box 
+    size="large" 
+    style={{backgroundColor: 'orange'}}
+  >
+    large orange box
+  </Box>
+)
+
+// function Box({className = '', style, ...otherProps}) {
+//   return <div className={`box ${className}`} style={{fontStyle: 'italic', ...style}} {...otherProps} />
+// }
+
+function Box({size, className = '', style, ...otherProps}) {
+  const sizeClassName = size ? `box--${size}` : '';
+
+  return <div className={`box ${className} ${sizeClassName}`} style={{fontStyle: 'italic', ...style}} {...otherProps} />
+}
 
 function App() {
   return (
@@ -22,6 +78,7 @@ function App() {
       {smallBox}
       {mediumBox}
       {largeBox}
+      <Box>sizeless box</Box>
     </div>
   )
 }
--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,10 +1,26 @@
 // Basic Forms
 // http://localhost:3000/isolated/exercise/06.js
 
-import React from 'react'
+import React from 'react';
 
 function UsernameForm({onSubmitUsername}) {
   // 🐨 add a submit event handler here (`handleSubmit`).
+  const usernameInputRef = React.useRef(null);
+  const [username, setUsername] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // onSubmitUsername(event.target.elements[0].value);
+    // onSubmitUsername(event.target.elements.usernameInput.value);
+    // onSubmitUsername(usernameInputRef.current.value); // useRef
+    onSubmitUsername(username); // useState
+  }
+
+  function handleChange(event) {
+    const {value} = event.target;
+    setUsername(value.toLowerCase());
+  }
+
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
@@ -19,10 +35,10 @@ function UsernameForm({onSubmitUsername}) {
   // 🐨 make sure to associate the label to the input by specifying an `id` on
   // the input and a matching value as an `htmlFor` prop on the label.
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="usernameInput">Username:</label>
+        <input ref={usernameInputRef} type="text" onChange={handleChange} value={username}/>
       </div>
       <button type="submit">Submit</button>
     </form>
